406fdc0c11debfbb7a332ab237e32423518cc280
AlirezaAhmadyan <ahmadyalireza111@gmail.com>
2024-04-11 20:56:08 +0430

    create-RequestLoan_process

--- script.js
+++ script.js
@@ -1,48 +1,68 @@
 'use strict';
 
 // Data
-const currencies = new Map([
-    ['USD', '&#36;'],
-    ['EUR', '&euro;'],
-    ['GBP', '&pound;'],
-])
-const accounts = new Map([
-    ["js", {
+const currencies = {
+    USD: '&#36;',
+    EUR: '&euro;',
+    GBP: '&pound;'
+}
+const accounts = {
+    js: {
         owner: 'Jonas Schmedtmann',
         currencyBalance: 'USD',
-        movements: new Map([[200, '01/03/2000'], [-400, '08/03/2024'], [3000, '05/01/2024'], [-650, '09/02/2024'], [-130, '17/02/2022'], [70, '30/03/2020']]),
+        movements: new Map([
+            [200, '01/03/2000'],
+            [-400, '08/03/2024'],
+            [3000, '05/01/2024'],
+            [-650, '09/02/2024'],
+            [-130, '17/02/2022'],
+            [70, '30/03/2020'],
+        ]),
         interestRate: 1.2, // %
         pin: 1111
-    }],
-    ["jd", {
+    },
+    jd: {
         owner: 'Jessica Davis',
         currencyBalance: 'USD',
-        movements: new Map([[5000, '01/03/2000'], [3400, '12/02/2020'], [-3210, '09/02/2024'], [-1000, '17/02/2020'], [8500, '30/03/2020'], [-30, '10/03/2024']]),
+        movements: new Map([
+            [5000, '01/03/2000'],
+            [3400, '12/02/2020'],
+            [-3210, '09/02/2024'],
+            [-1000, '17/02/2020'],
+            [8500, '30/03/2020'],
+            [-30, '10/03/2024']
+        ]),
         interestRate: 1.5,
         pin: 2222,
-    }],
-    ["stw", {
+    },
+    stw: {
         owner: 'Steven Thomas Williams',
         currencyBalance: 'USD',
-        movements: new Map([[200, '01/03/2000'], [-200, '10/02/2020'], [-20, '09/02/2024'], [50, '17/02/2022'], [400, '30/03/2020'], [-460, '10/03/2024']]),
+        movements: new Map([
+            [200, '01/03/2000'],
+            [-200, '10/02/2020'],
+            [-20, '09/02/2024'],
+            [50, '17/02/2022'],
+            [400, '30/03/2020'],
+            [-460, '10/03/2024']
+        ]),
         interestRate: 0.7,
         pin: 3333,
-    }]
-    , ["ss", {
+    },
+    ss: {
         owner: 'Sarah Smith',
         currencyBalance: 'USD',
-        movements: new Map([[430, '01/03/2000'], [1000, '05/01/2024'], [700, '09/02/2024'], [50, '30/03/2020'], [90, '10/03/2024']]),
+        movements: new Map([
+            [430, '01/03/2000'],
+            [1000, '05/01/2024'],
+            [700, '09/02/2024'],
+            [50, '30/03/2020'],
+            [90, '10/03/2024']
+        ]),
         interestRate: 1,
         pin: 4444,
-    }]
-])
-const updateData = function () {
-    sessionStorage.setItem(
-        'accounts',
-        JSON.stringify(Array.from(accounts.entries()))
-    )
+    }
 }
-updateData();
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -55,18 +75,12 @@ const labelTimer = document.querySelector('.timer');
 
 const containerApp = document.querySelector('.app');
 const containerMovements = document.querySelector('.movements');
+const btnFormLoan = document.querySelector('.form--loan .form__btn--loan');
+const inputFormLoan = document.querySelector('.form--loan .form__input--loan-amount');
 
 const btnsSort = document.querySelectorAll('.btn--sort');
 const btnSortAsc = document.querySelector('.btn--sort.btn--sort__ascending');
 const btnSortDesc = document.querySelector('.btn--sort.btn--sort__descending');
-
-const inputLoginUsername = document.querySelector('.login__input--user');
-const inputLoginPin = document.querySelector('.login__input--pin');
-const inputTransferTo = document.querySelector('.form__input--to');
-const inputTransferAmount = document.querySelector('.form__input--amount');
-const inputLoanAmount = document.querySelector('.form__input--loan-amount');
-const inputCloseUsername = document.querySelector('.form__input--user');
-const inputClosePin = document.querySelector('.form__input--pin');
 
 const dateNow = (splitChar = '/') => {
     const date = new Date();
@@ -91,7 +105,7 @@ const getData = function (account) {
                     balance *= 0.92;
                     break;
             }
-            return balance.toFixed(2) + currencies.get(currency);
+            return balance.toFixed(2) + currencies[currency];
         },
         sumIn() {
             return this.balance(movementsArr.filter(value => value > 0));
@@ -124,12 +138,15 @@ const getData = function (account) {
                 `;
                 containerMovements.insertAdjacentHTML('afterbegin', movementContent);
             })
+        },
+        addBalance(amount) {
+            account.movements.set(parseFloat(amount), dateNow());
         }
     }
 }
 
 //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ Event Scripts ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
-const currentUser = getData(accounts.get('js'));
+const currentUser = getData(accounts["js"]);
 labelBalance.innerHTML = currentUser.balance();
 labelDate.innerHTML = dateNow();
 labelWelcome.innerHTML = `Welcome back, ${currentUser.firstName}`;
@@ -156,4 +173,11 @@ btnsSort.forEach(btnSort => {
             }
         }
     })
+})
+btnFormLoan.addEventListener('click', (e) => {
+    e.preventDefault();
+    const loanAmount = inputFormLoan.value;
+    parseFloat(loanAmount) > 0 && currentUser.addBalance(loanAmount);
+    inputFormLoan.value = "";
+    currentUser.showMovements();
 })
